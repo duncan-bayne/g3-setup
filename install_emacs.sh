52327f047f329fb8341549cc2406b90033f25271
Duncan Bayne <dhgbayne@fastmail.fm>
2014-03-25 16:23:45 +1100

    Use correctly named temp path.

--- install_emacs.sh
+++ install_emacs.sh
@@ -5,9 +5,9 @@ echo
 echo Downloading and installing Emacs from source
 echo
 
-rm -rf ~/mint_setup_tmp/emacs
-mkdir ~/mint_setup_tmp/emacs
-pushd ~/mint_setup_tmp/emacs
+rm -rf ~/g3_setup_tmp/emacs
+mkdir ~/g3_setup_tmp/emacs
+pushd ~/g3_setup_tmp/emacs
 wget http://mirror.aarnet.edu.au/pub/gnu/emacs/emacs-24.3.tar.gz
 tar -zxvf emacs-24.3.tar.gz
 cd emacs-24.3
@@ -15,4 +15,4 @@ cd emacs-24.3
 make
 make install
 popd
-rm -rf ~/mint_setup_tmp/emacs
+rm -rf ~/g3_setup_tmp/emacs
--- install_sbcl.sh
+++ install_sbcl.sh
@@ -6,11 +6,11 @@ echo Build SBCL from source, so we are using a good version with threading enabl
 
 apt-get -y install clisp
 
-rm -rf ~/mint_setup_tmp/sbcl
-mkdir -p ~/mint_setup_tmp/sbcl
+rm -rf ~/g3_setup_tmp/sbcl
+mkdir -p ~/g3_setup_tmp/sbcl
 curl "http://aarnet.dl.sourceforge.net/project/sbcl/sbcl/1.1.16/sbcl-1.1.16-source.tar.bz2" > sbcl-source.tar.bz2
-cp sbcl-source.tar.bz2 ~/mint_setup_tmp/sbcl
-pushd ~/mint_setup_tmp/sbcl
+cp sbcl-source.tar.bz2 ~/g3_setup_tmp/sbcl
+pushd ~/g3_setup_tmp/sbcl
 bunzip2 sbcl-source.tar.bz2
 tar -xvf sbcl-source.tar
 cd sbcl-1.1.16
@@ -19,5 +19,5 @@ INSTALL_ROOT=/usr/local sh install.sh
 popd
 
 rm -f sbcl-source.tar.bz2
-rm -rf ~/mint_setup_tmp/sbcl
+rm -rf ~/g3_setup_tmp/sbcl
 apt-get purge -y clisp
